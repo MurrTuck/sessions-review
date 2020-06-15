001760c5a7bd85d65a999c69dc273f91b66b8a42
MurrTuck <murrtuck@gmail.com>
2020-06-15 12:28:27 -0600

    Added Sessions

--- server/controllers/moviesController.js
+++ server/controllers/moviesController.js
@@ -1,12 +1,20 @@
 module.exports = {
   getAllMovies: async (req, res) => {
     const db = req.app.get('db')
+    const { rating, year } = req.query
 
     const movies = await db.movies.find()
 
-    res.status(200).send(movies)
-  },
+    if (rating) {
+      const filteredMovies = movies.filter((movie) => {
+        return movie.rating > +rating
+      })
 
+      res.status(200).send(filteredMovies)
+    } else {
+      res.status(200).send(movies)
+    }
+  },
   getMovieById: async (req, res) => {
     const db = req.app.get('db')
     const { id } = req.params
@@ -24,23 +32,21 @@ module.exports = {
       res.status(500).send('Did a boo boo')
     }
   },
-
-
   addMovie: async (req, res) => {
     const db = req.app.get('db')
 
     const { title, rating, year } = req.body
 
-    const [newMovie] = db.movies.insert({ title, rating, year })
+    const newMovie = await db.movies.insert({ title, rating, year })
 
     res.status(200).send(newMovie)
   },
-
   deleteMovie: async (req, res) => {
     const db = req.app.get('db')
 
     const { id } = req.params
+
     await db.movies.destroy({ id })
-    res.status(200).send('Deleted Movie')
+    res.status(200).send('Deleted movie')
   },
 }
